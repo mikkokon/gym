dd38c96ba6186f8cff9512aa8f125b3dd14184c9
Mikko <mikko@hrsuunti.fi>
2020-12-15 18:55:01 +0200

    feat: Add firebase stuffs

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyByCbvDZsEfCMww0ZGyhxVC8phkWWRA9Aw",
+    authDomain: "fir-react-gym.firebaseapp.com",
+    projectId: "fir-react-gym",
+    storageBucket: "fir-react-gym.appspot.com",
+    messagingSenderId: "446837920028",
+    appId: "1:446837920028:web:30bbbcc4dabbdffc7aead9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
