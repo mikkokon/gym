dea69811323f9a46201c699387de0f3da439415e
Mikko <mikko@hrsuunti.fi>
2020-12-29 20:11:57 +0200

    upd

--- src/components/NumberPad/NumberPad.tsx
+++ src/components/NumberPad/NumberPad.tsx
@@ -33,13 +33,13 @@ const NumberPad = (props:INumberPadProps) => {
         <div onClick={() => handleNumber('1')}>1</div>
         <div onClick={() => handleNumber('2')}>2</div>
         <div onClick={() => handleNumber('3')}>3</div>
-        <div onClick={() => handleNumber(4)}>4</div>
-        <div onClick={() => handleNumber(5)}>5</div>
-        <div onClick={() => handleNumber(6)}>6</div>
-        <div onClick={() => handleNumber(7)}>7</div>
-        <div onClick={() => handleNumber(8)}>8</div>
-        <div onClick={() => handleNumber(9)}>9</div>
-        <div onClick={() => handleNumber(0)}>0</div>
+        <div onClick={() => handleNumber('4')}>4</div>
+        <div onClick={() => handleNumber('5')}>5</div>
+        <div onClick={() => handleNumber('6')}>6</div>
+        <div onClick={() => handleNumber('7')}>7</div>
+        <div onClick={() => handleNumber('8')}>8</div>
+        <div onClick={() => handleNumber('9')}>9</div>
+        <div onClick={() => handleNumber('0')}>0</div>
         <div onClick={() => setNumbers('')}>C</div>
         <div className='ok' onClick={() => setNumbers('')}>v</div>
       </div> 
